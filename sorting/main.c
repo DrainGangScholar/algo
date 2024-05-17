40c719a952dc89854030f35e20ee1ee2b58a95c5
Veljko <veljko.markovic@atessoft.rs>
2024-05-17 15:52:36 +0200

    D:::

--- sorting/main.c
+++ sorting/main.c
@@ -1,12 +1,10 @@
 #include "include/bubble_sort.h"
 #include "include/common.h"
 #include "include/heap.h"
-#define NUMBER 16
-int main(void) {
-  int *array = init_array(NUMBER, 1, 100);
-  heap *h = min_create_heap(NUMBER, NUMBER, array);
-  //heap_sort(h, INCREASING);
-  print_array(h->array, h->size);
+int main(int argc, char *argv[]) {
+  int size = atoi(argv[1]);
+  int *array = init_array(size, 1, 100);
+  heap *h = min_create_heap(size, size, array);
+  print_array(&h->array[0], 1);
   printf("%d\n", extract_root(h, MIN_HEAP));
-  print_array(h->array, h->size);
 }
